0882dc5651f165b848a2cbc012eb93eb7c3938aa
SaqeebPatel <saqeeb3p@gmail.com>
2024-09-27 00:06:21 +0530

    add the file

--- Backend/middlware/multer.js
+++ Backend/middlware/multer.js
@@ -1,27 +1,4 @@
-// const multer = require('multer');
-// const path = require('path');
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'uploads/'); // Ensure this directory exists
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
-//   }
-// });
-
-// const fileFilter = (req, file, cb) => {
-//   const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
-//   if (allowedTypes.includes(file.mimetype)) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error('Invalid file type'), false);
-//   }
-// };
-
-// const upload = multer({ storage, fileFilter });
-
-// module.exports = upload;
 const multer = require("multer");
 const path = require("path");
 
